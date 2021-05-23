4ec213d1f8a33e3ce2021be42df4ae93e08aa9bf
Victor Chicu <victor@aroundwire.com>
2021-05-24 00:22:15 +0300

    Fixes model annotations.

--- src/main/java/com/cryptostrophe/bot/repository/model/SymbolTickerEvent.java
+++ src/main/java/com/cryptostrophe/bot/repository/model/SymbolTickerEvent.java
@@ -1,7 +1,13 @@
 package com.cryptostrophe.bot.repository.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "symbol_ticker_events")
 public class SymbolTickerEvent {
+    @Field("event_time")
     private Long eventTime;
+    @Field("participant_id")
     private Integer participantId;
 
     public Long getEventTime() {
