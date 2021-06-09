47f06e92bb8b4e75c88eb7c0b6fc16162430f73a
Victor Chicu <victor@aroundwire.com>
2021-06-09 10:10:25 +0300

    Adds compound indexes for participant subscription and symbol ticker event collections

---
+++ src/main/java/com/cryptostrophe/bot/configs/MongoIndexConfig.java
@@ -0,0 +1,40 @@
+package com.cryptostrophe.bot.configs;
+
+import com.cryptostrophe.bot.repository.model.ParticipantSubscriptionEntity;
+import com.cryptostrophe.bot.repository.model.SymbolTickerEventEntity;
+import org.bson.Document;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.CompoundIndexDefinition;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class MongoIndexConfig {
+    private final MongoOperations mongoOperations;
+
+    public MongoIndexConfig(MongoOperations mongoOperations) {
+        this.mongoOperations = mongoOperations;
+    }
+
+    @PostConstruct
+    public void init() {
+        mongoOperations.indexOps(ParticipantSubscriptionEntity.class)
+                .ensureIndex(
+                        new CompoundIndexDefinition(
+                                new Document()
+                                        .append("participantId", 1)
+                                        .append("symbol", 1)
+                        ).unique().sparse().named("participant_subscription")
+                );
+
+        mongoOperations.indexOps(SymbolTickerEventEntity.class)
+                .ensureIndex(
+                        new CompoundIndexDefinition(
+                                new Document()
+                                        .append("participantId", 1)
+                                        .append("symbol", 1)
+                        ).unique().sparse().named("symbol_ticker_event")
+                );
+    }
+}
