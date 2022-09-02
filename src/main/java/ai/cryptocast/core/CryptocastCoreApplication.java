08764ebe0f109ffe7c5e19155ab8806d5bacafd1
Victor Chicu <victorc@hellorep.com>
2022-09-02 22:40:21 +0300

    Gradle upgrade to 2.7.3 to support M1 apple processors

--- src/main/java/ai/cryptocast/core/CryptocastCoreApplication.java
+++ src/main/java/ai/cryptocast/core/CryptocastCoreApplication.java
@@ -3,10 +3,8 @@ package ai.cryptocast.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@EnableFeignClients
 @EnableMongoAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CryptocastCoreApplication {
